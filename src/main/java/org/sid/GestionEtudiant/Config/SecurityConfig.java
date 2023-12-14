85c2aee64c552bcdb7b35a217e9426b50aeef8e9
Fanta Coulibaly <fantacoulibaly101@gmail.com>
2023-12-14 00:52:10 +0000

    m

--- src/main/java/org/sid/GestionEtudiant/Config/SecurityConfig.java
+++ src/main/java/org/sid/GestionEtudiant/Config/SecurityConfig.java
@@ -8,10 +8,14 @@ import org.sid.GestionEtudiant.Service.Impl.DetailsUtilisateurServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -29,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-                .requestMatchers("/public/**",  "/change-password").permitAll() // Autoriser l'accès à ces pages sans authentification
+                .requestMatchers("/login", "/change-password").permitAll() // Autoriser l'accès à ces pages sans authentification
                 .anyRequest().authenticated()
             .and()
             .formLogin()
@@ -41,9 +45,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login") // Redirection après une déconnexion réussie
                 .permitAll();
     }
-
-    // Ajoutez votre gestionnaire d'authentification et encodeur ici
-
+    
+    
+    
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().requestMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
@@ -55,9 +59,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
             .passwordEncoder(passwordEncoder());
-      
-
-         }
+    }
+    
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() {
+        return super.authenticationManagerBean();
+    }
 
     
 
@@ -65,6 +73,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    public void exampleAuthentication() {
+        // Notez que vous ne devriez pas utiliser directement l'AuthenticationManager de cette manière
+        // Il est généralement utilisé automatiquement par Spring Security lors de l'authentification
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String motDePasseNonEncode = "mot_de_passe";
+        String motDePasseEncode = encoder.encode(motDePasseNonEncode);
+
+        // Création d'un objet Authentication
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("nom_utilisateur", motDePasseNonEncode);
+
+        // Obtention de l'AuthenticationManager
+        AuthenticationManager authenticationManager = null;
+		try {
+			authenticationManager = authenticationManagerBean();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+        // Authentification
+        try {
+            authenticationManager.authenticate(authenticationToken);
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- src/main/java/org/sid/GestionEtudiant/Config/WebSecurityConfigurerAdapter.java
+++ src/main/java/org/sid/GestionEtudiant/Config/WebSecurityConfigurerAdapter.java
@@ -1,6 +1,8 @@
 package org.sid.GestionEtudiant.Config;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 
@@ -20,5 +22,12 @@ public class WebSecurityConfigurerAdapter {
 		// TODO Auto-generated method stub
 		
 	}
+
+
+	public AuthenticationManager authenticationManagerBean() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
